e33ea4f85ec5551e50b92362099673f644ac564d
Trailblazer_dev <richvictor830@gmail.com>
2025-09-11 17:20:46 +0300

    almost done

--- advanced_python/Projects/Learning_Log/accounts/urls.py
+++ advanced_python/Projects/Learning_Log/accounts/urls.py
@@ -1,8 +1,14 @@
 """Defines URL patterns for accounts."""
 from django.urls import path,include
-
+from . import views
+from django.contrib.auth.views import LogoutView
 app_name = 'accounts'
 urlpatterns = [
     # include default auth urls
     path('',include('django.contrib.auth.urls')),
+    # Registration Page
+    path('register/',views.register,name='register'),
+    # Logout Page
+    path('logout/',LogoutView.as_view(),name='logout'),
+    
 ]
--- advanced_python/Projects/Learning_Log/ll_project/settings.py
+++ advanced_python/Projects/Learning_Log/ll_project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'learning_logs',
-    'accounts'
+    'accounts',
     # Default Django apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# My settings 
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
